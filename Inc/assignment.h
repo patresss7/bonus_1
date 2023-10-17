c491e59cd2749468f770e9a70af0371e3e961d78
patresss7 <75572905+patresss7@users.noreply.github.com>
2023-10-17 10:55:31 +0200

    Macros added

--- Inc/assignment.h
+++ Inc/assignment.h
@@ -1,50 +1,55 @@
-/*
- * assignment.h
- *
- *  Created on: 27. 9. 2020
- *      Author: Stancoj
- */
-
 #ifndef ASSIGNMENT_H_
 #define ASSIGNMENT_H_
 
+
 /**
- * 		This header file provides macros to the MCU's registers required for this assignment.
- * 		Your task is to provide their actual implementation so you can use them in application in "main.c"
- * 		and make your "LED blink" application code readable and great again!
+ * This header file provides macros to the MCU's registers required for this assignment.
+ * Your task is to provide their actual implementation so you can use them in the application in "main.c"
+ * and make your "LED blink" application code readable and great again!
  */
 
 /* General purpose input output port A macros */
 //GPIOA peripheral base address
-#define	GPIOA_BASE_ADDR			/* Add GPIO A peripheral base address here. */
+#define	GPIOA_BASE_ADDR			(uint32_t)(0x48000000U)
 //MODER register
-#define	GPIOA_MODER_REG			/* Add moder register address here. */
+#define	GPIOA_MODER_REG			(uint32_t *)(GPIOA_BASE_ADDR + 0x00U)
 //OTYPER register
-#define	GPIOA_OTYPER_REG		/* Add otyper register address here. */
+#define	GPIOA_OTYPER_REG		(uint32_t *)(GPIOA_BASE_ADDR + 0x04)
 //OSPEEDER register
-#define GPIOA_OSPEEDER_REG		/* Add ospeeder register address here. */
+#define GPIOA_OSPEEDER_REG		(uint32_t *)(GPIOA_BASE_ADDR + 0x08U)
 //PUPDR register
-#define GPIOA_PUPDR_REG			/* Add pupdr register address here. */
+#define GPIOA_PUPDR_REG			(uint32_t *)(GPIOA_BASE_ADDR + 0x0CU)
 //IDR register
-#define GPIOA_IDR_REG			/* Add idr register address here. */
+#define GPIOA_IDR_REG			(uint32_t *)(GPIOA_BASE_ADDR + 0x10U)
 //ODR register
-#define GPIOA_ODR_REG			/* Add odr register address here. */
+#define GPIOA_ODR_REG			(uint32_t *)(GPIOA_BASE_ADDR + 0x14U)
 //BSRR register
-#define GPIOA_BSRR_REG			/* Add bsrr register address here. */
+#define GPIOA_BSRR_REG			(uint32_t *)(GPIOA_BASE_ADDR + 0x18U)
 //BRR register
-#define GPIOA_BRR_REG			/* Add brr register address here. */
+#define GPIOA_BRR_REG			(uint32_t *)(GPIOA_BASE_ADDR + 0x28U)
 
 /*Reset clock control register macros */
 //RCC base address
-#define	RCC_BASE_ADDR			/* Add rcc register address here. */
+#define	RCC_BASE_ADDR			(uint32_t)(0x40021000U)
 //AHBEN register
-#define	RCC_AHBENR_REG			/* Add ahben register address here. */
+#define	RCC_AHBENR_REG			*((volatile uint32_t *) (uint32_t)(RCC_BASE_ADDR + 0x00000014U))
 
 /* LED and button macros */
-#define LED_ON					/* Add LED_ON implementation here. */
-#define LED_OFF					/* Add LED_OFF implementation here. */
+#define LED_PIN                 LL_GPIO_PIN_4  // Define the LED pin
+#define BUTTON_PIN              LL_GPIO_PIN_3  // Define the button pin
 
-#define BUTTON_GET_STATE		/* Add BUTTON_GET_STATE implementation here. */
+/* LED and button macros */
+#define LED_ON					*((volatile uint32_t *)((GPIOA_BSRR_REG))) |= (1<<4)
+#define LED_OFF					*((volatile uint32_t *)((GPIOA_BRR_REG))) |= (1<<4)
 
+#define BUTTON_GET_STATE		!(*((volatile uint32_t *)((uint32_t)(0x48000000 + 0x10U))) & (1 << 3))
+
+typedef enum {
+    NONE = 0,
+    RISE = 1,
+    FALL = 2
+} EDGE_TYPE;
+
+EDGE_TYPE edgeDetect(uint8_t pin_state, uint8_t samples);
 
 #endif /* ASSIGNMENT_H_ */
